439fb3957b5e964fb4dada6e5b070641919b42b9
Mitfry <mitchellhoutkamp@msn.com>
2017-10-27 23:47:04 +0200

    - Fix for offline test account. - Minor fixes
    **Final push for app submission!
    
    To-Do:
    Extra:
    - Add more comments to newest files.
    - Load server images (if possible).
    - Add static table header to aid viewing large amounts of data.
    - Add activity for user to change its own account details.
    - Load data per logged-in user.

--- app/src/main/java/s1080488/ikpmd_app/Models/RegisterRequest.java
+++ app/src/main/java/s1080488/ikpmd_app/Models/RegisterRequest.java
@@ -14,7 +14,7 @@ public class RegisterRequest extends StringRequest {
     private static final String REGISTER_REQUEST_URL = "https://www.dinodeluxe.eu/DinoDeluxe_App/register.php";
     private Map<String, String> params;
 
-    public RegisterRequest(String username, String email, String firstname, String lastname, String ign, String password, Response.Listener<String> listener) {
+    public RegisterRequest(String username, String email, String firstname, String lastname, String ign, String password, Response.Listener<String> listener){
         super(Method.POST, REGISTER_REQUEST_URL, listener, null);
         params = new HashMap<>();
         params.put("username", username);
@@ -22,7 +22,7 @@ public class RegisterRequest extends StringRequest {
         params.put("firstname", firstname);
         params.put("lastname", lastname);
         params.put("ign", ign);
-        params.put("u_password", password);
+        params.put("password", password);
     }
 
     @Override
